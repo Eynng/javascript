5056b53270419b5c0a118ff0ffe6b7598907b532
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-08-22 14:37:23 -0300

    Aula 11 – Condições (Parte 1) - JavaScript

---
+++ exercícios/modulo-d/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 678.2
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { //condição simples//
+        console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Diriga sempre usando cinto de segurança')
