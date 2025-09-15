e8171ff34e1a6d6cb34a2c46bed9c675ac743479
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-09-14 23:59:23 -0300

    Update script2.js

--- desafios/d003/script2.js
+++ desafios/d003/script2.js
@@ -12,12 +12,6 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        if (i > f) {
-            for (let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
-            }
-            res.innerHTML += `\u{1F3C1}`
-        }
+        
     }
 }
