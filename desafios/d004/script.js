1dcd8e770664bb54515256b48bae799810ddba3d
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-09-17 12:01:29 -0300

    Praticando Operações de repetições no JavaScript com o projeto Tabuada

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -1,4 +1,4 @@
-function tabuada() {
+/*function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
@@ -15,5 +15,49 @@ function tabuada() {
             c++
         }
     }
-    
-}
+} 
+*/
+
+    /*function tabuada() {
+        let num = document.getElementById('txtn')
+        let tab = document.getElementById('seltab')
+        tab.innerHTML = ''
+        if (num.value.length == 0) {
+            window.alert('[ERRO] digite um número!')
+        } else {
+            let n = Number(num.value)
+            let c = 1
+            while (c <= 10) {
+                let item = document.createElement('option')
+                item.text = `${n} x ${c} = ${n*c}`
+                tab.appendChild(item)
+                c++
+            }
+        }
+    } */
+
+        function tabuada() {
+            let num = document.getElementById('txtn')
+            let tab = document.getElementById('seltab')
+            tab.innerHTML = ''
+            if (num.value.length == 0) {
+                windown.alert('[ERRO] digite um número!')
+            } else {
+                let n = Number(num.value)
+                let c = 1
+                while (c <= 10) {
+                    let item = document.createElement('option')
+                    item.text =(`${n} x ${c} = ${n*c}`)
+                    tab.appendChild(item)
+                    c++
+                }
+            } 
+        }
+
+        function limpar() {
+            let txtn = document.getElementById('txtn')
+            let seltab = document.getElementById('seltab')
+            txtn.value = ''
+            seltab.innerHTML = ''
+            seltab.value = ''
+        }
