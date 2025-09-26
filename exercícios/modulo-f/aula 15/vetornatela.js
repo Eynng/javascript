f1fbc68249fc8419d7c55a4df373464845ea72f2
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-09-25 23:50:19 -0300

    Praticando JavaScript - ARRAY, VETORES

---
+++ exercícios/modulo-f/aula 15/vetornatela.js
@@ -0,0 +1,9 @@
+let valores = [8 , 1 , 7 , 4 , 2 , 9]
+
+valores.sort()
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
