4f10365e1340ab5b1ed9ed363b35298497519930
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-09-26 10:40:37 -0300

    Aula 15 – Variáveis Compostas - aprendendo conceitos de VETORES/ARRAY

--- exercícios/modulo-f/aula 15/ambiente.js
+++ exercícios/modulo-f/aula 15/ambiente.js
@@ -1,7 +1,13 @@
 let num = [5, 8, 2, 9, 3]
 
-num.sort()
 num.push(1)
+num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições `)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8) 
+if (pos < 0 ) {
+    console.log('O valor não foi encontrado no vetor')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
--- exercícios/modulo-f/aula 15/vetornatela.js
+++ exercícios/modulo-f/aula 15/vetornatela.js
@@ -1,9 +1,37 @@
-let valores = [8 , 1 , 7 , 4 , 2 , 9]
+let valores = [8 , 1 , 7 , 4 , 2 , 9 , 1 , 5 , 8 , 4 ] 
+
+
+               /* PERCURSO PRA EXIBIÇÃO DO VETOR */
 
 valores.sort()
-console.log(valores[0])
+/valores.push(3)
+valores.sort()
+console.log(valores)
+
+                /* JEITO ERRADO DE EXECUTAR UM ARRAY/VETOR */
+
+onsole.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
+console.log(valores[6])
+console.log(valores[7])
+console.log(valores[8])
+
+
+                   /* FORMA PADRÃO DE EXECUÇÃO DE UM ARRAY/VETOR */
+/*para*/
+ for(let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]} `)
+ }
+
+                    /* FORAM SIMPLIFICADA PRA EXIBIÇÃO DE UM ARRAY/VETOR */
+
+/*para cada posição em num (variavel composta)*/
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+}
+
