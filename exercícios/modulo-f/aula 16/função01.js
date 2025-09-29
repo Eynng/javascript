581c23812634c37697504371de80623c0edcbf8b
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-09-29 09:01:00 -0300

    Funções - JavaScript - Praticando

---
+++ exercícios/modulo-f/aula 16/função01.js
@@ -0,0 +1,19 @@
+function parimp(n) {
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+
+}
+
+// 1' Metodos de função
+
+let res = parimp(24)
+console.log(res)
+
+
+// 2' Metodos de função
+
+
+console.log(parimp(12))
