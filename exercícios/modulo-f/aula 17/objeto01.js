ff8d17ae380a9432d1e3d727959930a951176ef2
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-10-04 00:39:51 -0300

    Update objeto01.js

--- exercícios/modulo-f/aula 17/objeto01.js
+++ exercícios/modulo-f/aula 17/objeto01.js
@@ -1,4 +1,4 @@
-let amigo = {nome: 'José',
+/*let amigo = {nome: 'José',
 sexo: 'M',
 peso: 85.4,
 engordar(p=0){
@@ -9,4 +9,14 @@ engordar(p=0){
 }}
 
 amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)*/
+
+let amigo = {nome: 'Marcos',
+    sexo: 'M',
+    peso: 90.5
+
+    engordar(p=0) {
+    console.log('Engordou')
+    this.peso += p
+}
+}
