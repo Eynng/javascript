e4a0439f17fc1ec3eb368679802f83da5d72ded5
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-11-07 19:06:42 -0300

    Praticando JavaScript

--- exercícios/modulo-d/aula11/ex008.js
+++ exercícios/modulo-d/aula11/ex008.js
@@ -1,6 +1,22 @@
-var vel = 678.2
-console.log(`A velocidade do seu carro é de ${vel}Km/h`)
-if (vel > 60) { //condição simples//
-        console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+// var vel = 678.2
+// console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+// if (vel > 60) { //condição simples//
+//         console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+// }
+// console.log('Diriga sempre usando cinto de segurança')
+
+var vel = 59.9
+console.log(`Sua velocidade atual é de ${vel} Km/h`)
+if (vel > 60) {
+        console.log(`Você atengiu a velocidade mínima permitida, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
 }
-console.log('Diriga sempre usando cinto de segurança')
+
+var vell = 50 
+console.log(`Sua velocidade atual é de ${vell} Km/h`)
+if (vell > 60 ) {
+        console.log(`Sua velocidade atual é de ${vell} Km/h, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
+}
