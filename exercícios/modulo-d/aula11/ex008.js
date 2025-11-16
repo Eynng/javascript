bfc8971a19959c2edf08de67c047f7a02fd901a0
Esdras Lira <122489986+Eynng@users.noreply.github.com>
2025-11-15 23:47:30 -0300

    Praticando JavaScript

--- exercícios/modulo-d/aula11/ex008.js
+++ exercícios/modulo-d/aula11/ex008.js
@@ -20,3 +20,33 @@ if (vell > 60 ) {
 } else {
         console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
 }
+var vel = 59.9
+console.log(`Sua velocidade atual é de ${vel} Km/h`)
+if (vel > 60) {
+        console.log(`Você atengiu a velocidade mínima permitida, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
+}
+
+var vell = 50 
+console.log(`Sua velocidade atual é de ${vell} Km/h`)
+if (vell > 60 ) {
+        console.log(`Sua velocidade atual é de ${vell} Km/h, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
+}
+var vel = 59.9
+console.log(`Sua velocidade atual é de ${vel} Km/h`)
+if (vel > 60) {
+        console.log(`Você atengiu a velocidade mínima permitida, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
+}
+
+var vell = 50 
+console.log(`Sua velocidade atual é de ${vell} Km/h`)
+if (vell > 60 ) {
+        console.log(`Sua velocidade atual é de ${vell} Km/h, MULTADO!`)
+} else {
+        console.log('Diriga-se sempre com sinto de segurança e com cuidado, boa viajem!')
+}
